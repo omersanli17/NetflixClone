682d4a69f54b8b83433c0e7855dd788666fd9987
Omer <sanli.omer@metu.edu.tr>
2024-02-27 21:14:41 +0300

    Add: Test no storyboard, main bg as yellow color

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabBarViewController.swift
 //  NetflixClone
 //
 //  Created by ömer şanlı on 27.02.2024.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
     }
 
 
