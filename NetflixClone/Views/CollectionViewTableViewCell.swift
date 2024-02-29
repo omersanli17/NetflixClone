1702ad382fc7796e3665229cae9014cdc10c8caf
Omer <sanli.omer@metu.edu.tr>
2024-02-29 14:27:59 +0300

    Add: Register TableView into CollectionViewCell

---
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  NetflixClone
+//
+//  Created by ömer şanlı on 29.02.2024.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
