5b3732997284ac8f30921dce168781f057fcd446
Omer <sanli.omer@metu.edu.tr>
2024-03-01 13:58:54 +0300

    Add: Download Button

--- NetflixClone/Views/HeroHeaderUIView.swift
+++ NetflixClone/Views/HeroHeaderUIView.swift
@@ -9,12 +9,23 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
 
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+
     private let playButton: UIButton = {
         let button = UIButton()
         button.setTitle("Play", for: .normal)
         button.layer.borderColor = UIColor.white.cgColor
         button.layer.borderWidth = 1
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
         return button
     }()
 
@@ -31,6 +42,7 @@ class HeroHeaderUIView: UIView {
         addSubview(heroImageView)
         addGradient()
         addSubview(playButton)
+        addSubview(downloadButton)
         applyConstraints()
     }
 
