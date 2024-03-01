3121e1d9e496736421693f10a0464f6fc3461f45
Omer <sanli.omer@metu.edu.tr>
2024-03-01 13:59:05 +0300

    Edit: Constraints of buttons

--- NetflixClone/Views/HeroHeaderUIView.swift
+++ NetflixClone/Views/HeroHeaderUIView.swift
@@ -48,11 +48,19 @@ class HeroHeaderUIView: UIView {
 
     private func applyConstraints() {
         let playButtonConstraints = [
-            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 90),
-            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
-            playButton.widthAnchor.constraint(equalToConstant: 100)
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 70),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            playButton.widthAnchor.constraint(equalToConstant: 120)
         ]
+
+        let downloadButtonConstraints = [
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -70),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+
         NSLayoutConstraint.activate(playButtonConstraints)
+        NSLayoutConstraint.activate(downloadButtonConstraints)
     }
 
     private func addGradient() {
