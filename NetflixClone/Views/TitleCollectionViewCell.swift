ebb39a6e6ccfc06f7041b1e7e95bb778a0a8e479
Omer <sanli.omer@metu.edu.tr>
2024-03-02 10:03:57 +0300

    Add: Inject MoverServiceable

---
+++ NetflixClone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TitleCollectionViewCell.swift
+//  NetflixClone
+//
+//  Created by ömer şanlı on 2.03.2024.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+}
