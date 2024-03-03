eeb3691d9134ab623ef7b044fd169954ba1a385a
Omer <sanli.omer@metu.edu.tr>
2024-03-03 12:56:32 +0300

    Add: Insert API Key

--- NetflixClone/Utilities/Constants/Constant+API.swift
+++ NetflixClone/Utilities/Constants/Constant+API.swift
@@ -9,6 +9,6 @@ import Foundation
 
 extension Constant {
     struct API {
-        static let API_KEY = "641cc5f760eb15356d991637786f7aab"
+        static let API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NDFjYzVmNzYwZWIxNTM1NmQ5OTE2Mzc3ODZmN2FhYiIsInN1YiI6IjY1ZTFkNmNjYTM5ZDBiMDE4NjA5ZWQ3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tGwXSlr17LvsM_Q7HHLoTJOkSwFdLT9vhiFh4CooS5M"
     }
 }
